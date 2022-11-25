5e7ab8880b6229d35f450aee3280c53b96429feb
Christian Lane <73788721+ssdtac@users.noreply.github.com>
2022-11-24 21:30:57 -0500

    can now change number of cells

--- js/LFSR.js
+++ js/LFSR.js
@@ -3,7 +3,7 @@ $(function () {
 
 
 
-    $("#calculate").on('click', function() {
+    $("#calculate").click(function() {
         let x = "";
         $(".btn_content button").each(function(i, element) {
             x =  x.concat(element.innerHTML);
@@ -11,11 +11,20 @@ $(function () {
         console.log(x);
     })
 
-    $(".btn_content button").each(function () {
-        this.addEventListener("click", function () {
-            $(this).html() === '1' ? $(this).html('0')  : $(this).html('1');
+
+    $("#reload").click(function() {
+        $("tr").empty();
+        for(i=0; i<$("#cells").val(); i++) {
+        $("tr").append('<td><div class="btn_content"><button type="button" class="btn btn-outline-dark rounded-0 ">0</button></div></td>')
+        } 
+        console.log('yes')
+        $(".btn_content button").each(function () {
+            this.addEventListener("click", function () {
+                $(this).html() === '1' ? $(this).html('0')  : $(this).html('1');
+            });
         });
-    });
+    })
+    
 
 })
 
